c0ad022a4dc81d6567af59bc1a103c43a20a1537
medied <medina.eag@gmail.com>
2018-05-26 17:44:44 -0300

    Update to setup server

--- app.js
+++ app.js
@@ -1,40 +1,10 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
 let slideIndex = 1;
-showSlides(slideIndex);
 
-const cedulaInput = document.getElementById('cedula-input');
-const stringInput = document.getElementById('string-input');
-cedulaInput.addEventListener('change', (e) => uploadCedulaImg(e.target.files));
-stringInput.addEventListener('change', (e) => uploadStringImg(e.target.files));
-
-let data = {};
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
+let data = {}; // placeholder
 
 function connectWithUport() {
   console.log("connectWithUport() invoked");
@@ -51,3 +21,6 @@ function uploadStringImg(img) {
   data.stringImg = img;
   console.log(data);
 }
+
+app.use(express.static('public'))
+app.listen(8080);
---
+++ public/client.js
@@ -0,0 +1,35 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+const cedulaInput = document.getElementById('cedula-input');
+const stringInput = document.getElementById('string-input');
+cedulaInput.addEventListener('change', (e) => uploadCedulaImg(e.target.files));
+stringInput.addEventListener('change', (e) => uploadStringImg(e.target.files));
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
