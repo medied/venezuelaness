f9856ffa17cf09676ae765c82f42ffc6498729b0
medied <medina.eag@gmail.com>
2018-05-27 08:32:28 -0300

    Upload

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
+const fileUpload = require('express-fileupload');
 var morgan = require('morgan');
 var app = express();
 var path = require('path');
 var cors = require('cors');
-const fileUpload = require('express-fileupload');
 const connect = require('./db/connect.js');
 const db = require('./db/db.js');
 
@@ -59,13 +59,23 @@ app.post('/upload', function(req, res) {
     return res.status(400).send('No files were uploaded.');
   
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  let sampleFile = req.files.sampleFile;
+  let cedulaImg = req.files.cedulaImg;
+  console.log('TEST ', req.files.cedulaImg);
+
+  // db.AddVerificationPhotoPath(state.currentUserUPortAddress, cedulaImg.name, function (err) {
+  //   if (err) {
+  //     return res.status(500).send(err);
+  //   }
+  //   res.send('File uploaded!');
+  // })
  
   // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv(__dirname + '/public/images/' + fileName + '.jpg', function(err) {
+  cedulaImg.mv(__dirname + './public/images/' + cedulaImg.name + '.jpg', function(err) {
     if (err)
       return res.status(500).send(err);
- 
-    res.send('File uploaded!');
+    
+    // res.send('File uploaded!');
+    res.sendFile(__dirname + '/index.html');
+    res.end();
   });
 });
